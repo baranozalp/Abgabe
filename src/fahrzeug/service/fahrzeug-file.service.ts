bea9055754aed1f644ba9394470b3282960423a1
Valentin Gant <valentingant@yahoo.com>
2021-04-15 21:18:06 +0200

    index.ts + fahrzeug-file.service.ts added

---
+++ src/fahrzeug/service/fahrzeug-file.service.ts
@@ -0,0 +1,185 @@
+/**
+ * Das Modul besteht aus der Klasse {@linkcode FahrzeugFileService}, damit
+ * Binärdateien mit dem Treiber von _MongoDB_ in _GridFS_ abgespeichert und
+ * ausgelesen werden können.
+ * @packageDocumentation
+ */
+
+import { FahrzeugNotExists, FileNotFound, MultipleFiles } from './errors';
+import { closeMongoDBClient, connectMongoDB, logger, save } from '../../shared';
+import { FahrzeugModel } from '../entity';
+import { FahrzeugService } from './fahrzeug.service';
+import { GridFSBucket } from 'mongodb';
+import type { ObjectId } from 'mongodb';
+import { Readable } from 'stream';
+
+/* eslint-disable unicorn/no-useless-undefined */
+/**
+ * Mit der Klasse {@linkcode FahrzeugFileService} können Binärdateien mit dem
+ * Treiber von _MongoDB_ in _GridFS_ abgespeichert und ausgelesen werden.
+ */
+export class FahrzeugFileService {
+    private readonly service = new FahrzeugService();
+
+    /**
+     * Asynchrones Abspeichern einer Binärdatei.
+     *
+     * @param id ID des zugehörigen Fahrzeuges, die als Dateiname verwendet wird.
+     * @param buffer Node-Buffer mit den Binärdaten.
+     * @param contentType MIME-Type, z.B. `image/png`.
+     * @returns true, falls die Binärdaten abgespeichert wurden. Sonst false.
+     */
+    async save(id: string, buffer: Buffer, contentType: string | undefined) {
+        logger.debug(
+            'FahrzeugFileService.save(): id=%s, contentType=%s',
+            id,
+            contentType,
+        );
+
+        // Gibt es ein Fahrzeug zur angegebenen ID?
+        // eslint-disable-next-line line-comment-position, spaced-comment
+        const fahrzeug = await this.service.findById(id); //NOSONAR
+        if (fahrzeug === undefined) {
+            return false;
+        }
+
+        const { db, client } = await connectMongoDB();
+        const bucket = new GridFSBucket(db);
+        await this.deleteFiles(id, bucket);
+
+        // https://stackoverflow.com/questions/13230487/converting-a-buffer-into-a-readablestream-in-node-js#answer-44091532
+        const readable = new Readable();
+        // _read ist erforderlich, kann die leere Funktion sein
+        readable._read = () => {}; // eslint-disable-line no-underscore-dangle,no-empty-function
+        readable.push(buffer);
+        readable.push(null); // eslint-disable-line no-null/no-null, unicorn/no-null,  unicorn/no-array-push-push
+
+        const metadata = { contentType };
+        save(readable, bucket, id, { metadata }, client);
+        return true;
+    }
+
+    /**
+     * Asynchrones Suchen nach einer Binärdatei in _GridFS_ anhand des Dateinamens.
+     * @param filename Der Dateiname der Binärdatei.
+     * @returns GridFSBucketReadStream, falls es eine Binärdatei mit dem
+     *  angegebenen Dateinamen gibt. Im Fehlerfall ein JSON-Objekt vom Typ:
+     * - {@linkcode FahrzeugNotExists}
+     * - {@linkcode FileNotFound}
+     * - {@linkcode MultipleFiles}
+     */
+    async find(filename: string) {
+        logger.debug('FahrzeugFileService.findFile(): filename=%s', filename);
+        const resultCheck = await this.checkFilename(filename);
+        if (resultCheck !== undefined) {
+            return resultCheck;
+        }
+
+        const { db, client } = await connectMongoDB();
+
+        // https://mongodb.github.io/node-mongodb-native/3.5/tutorials/gridfs/streaming
+        const bucket = new GridFSBucket(db);
+        const resultContentType = await this.getContentType(filename, bucket);
+        if (typeof resultContentType !== 'string') {
+            return resultContentType;
+        }
+
+        const contentType = resultContentType;
+        // https://mongodb.github.io/node-mongodb-native/3.5/tutorials/gridfs/streaming/#downloading-a-file
+        // https://www.freecodecamp.org/news/node-js-streams-everything-you-need-to-know-c9141306be93
+        const readStream = bucket
+            .openDownloadStreamByName(filename)
+            .on('end', () => closeMongoDBClient(client));
+        return { readStream, contentType };
+    }
+
+    private async deleteFiles(filename: string, bucket: GridFSBucket) {
+        logger.debug('FahrzeugFileService.deleteFiles(): filename=%s', filename);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/naming-convention
+        const idObjects: { _id: ObjectId }[] = await bucket
+            .find({ filename })
+            .project({ _id: 1 })
+            .toArray();
+        const ids = idObjects.map((obj) => obj._id);
+        logger.debug('FahrzeugFileService.deleteFiles(): ids=%o', ids);
+        ids.forEach((fileId) =>
+            bucket.delete(fileId, () =>
+                logger.debug(
+                    'FahrzeugFileService.deleteFiles(): geloeschte ID=%o',
+                    fileId,
+                ),
+            ),
+        );
+    }
+
+    private async checkFilename(filename: string) {
+        logger.debug('FahrzeugFileService.checkFilename(): filename=%s', filename);
+
+        // Gibt es ein Fahrzeug mit dem gegebenen "filename" als ID?
+        // eslint-disable-next-line line-comment-position, spaced-comment
+        const fahrzeug = await FahrzeugModel.findById(filename); //NOSONAR
+        // eslint-disable-next-line no-null/no-null
+        if (fahrzeug === null) {
+            const result = new FahrzeugNotExists(filename);
+            logger.debug(
+                'FahrzeugFileService.checkFilename(): FahrzeugNotExists=%o',
+                result,
+            );
+            return result;
+        }
+
+        logger.debug('FahrzeugFileService.checkFilename(): fahrzeug=%o', fahrzeug);
+
+        return undefined;
+    }
+
+    private async getContentType(filename: string, bucket: GridFSBucket) {
+        let files: { metadata: { contentType: string } }[];
+        try {
+            files = await bucket.find({ filename }).toArray(); // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+        } catch (err: unknown) {
+            logger.error('%o', err);
+            files = [];
+        }
+
+        switch (files.length) {
+            case 0: {
+                const error = new FileNotFound(filename);
+                logger.debug(
+                    'FahrzeugFileService.getContentType(): FileNotFound=%o',
+                    error,
+                );
+                return error;
+            }
+
+            case 1: {
+                const [file] = files;
+                if (file === undefined) {
+                    const error = new FileNotFound(filename);
+                    logger.debug(
+                        'FahrzeugFileService.getContentType(): FileNotFound=%o',
+                        error,
+                    );
+                    return error;
+                }
+                const { contentType }: { contentType: string } = file.metadata;
+                logger.debug(
+                    'FahrzeugFileService.getContentType(): contentType=%s',
+                    contentType,
+                );
+                return contentType;
+            }
+
+            default: {
+                const error = new MultipleFiles(filename);
+                logger.debug(
+                    'FahrzeugFileService.getContentType(): MultipleFiles=%o',
+                    error,
+                );
+                return new MultipleFiles(filename);
+            }
+        }
+    }
+}
+
+/* eslint-enable unicorn/no-useless-undefined */
---
+++ src/fahrzeug/service/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Das Modul besteht aus den Klassen {@linkcode FahrzeugFileService} und
+ * {@linkcode FahrzeugService}, um Fahrzeuge und ihre zugehörige Binärdateien in
+ * MongoDB abzuspeichern, auszulesen, zu ändern und zu löschen einschließlich
+ * der Klassen für die Fehlerbehandlung.
+ * @packageDocumentation
+ */
+
+export * from './fahrzeug-file.service';
+export * from './fahrzeug.service';
+export * from './errors';
